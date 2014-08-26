b157143e38eef20b856fec5a5c98d918f6f2e4a4
jmcerrejon <ulysess@gmail.com>
2014-08-26 13:30:00 +0200

    changes

--- betabeers.sh
+++ betabeers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 # Author: Jose Cerrejon Gonzalez
 #
@@ -9,7 +9,7 @@ clear
 # Capturamos las respuestas en el fichero siguiente
 
 INPUT=/tmp/mnu.sh.$$
-
+BTITLE="BetaBeers Huelva | Fecha: $(date '+%d/%m/%y') | José Manuel Cerrejón González | IP: $(hostname -I)"
 # Borramos el fichero anterior cuando salgamos
 
 trap "rm $INPUT; exit" SIGHUP SIGINT SIGTERM
@@ -26,20 +26,37 @@ RandomNum(){
     read -p "E N H O R A B U E N A - N Ú M E R O: ${RANDOM_NUM}"
 }
 
+RPLAY(){
+    dialog  --title     "RPlay" \
+        --backtitle "${BTITLE}" \
+        --yes-label "Encender" \
+        --no-label  "Apagar" \
+        --yesno     "¿Qué deseas hacer?" 7 60
+
+    response=$?
+    case $response in
+       0) sudo update-rc.d -f rplay enable ; sudo /etc/init.d/rplay start;;
+       1) sudo /etc/init.d/rplay stop ; sudo update-rc.d -f rplay disable;;
+       255) echo "[ESC] key pressed.";;
+    esac
+}
+
 # Main menu
 
 while true
 do
-    dialog --clear --backtitle "BetaBeers Huelva | Fecha: $(date '+%d/%m/%y') | José Manuel Cerrejón González | IP: $(hostname -I)" \
+    dialog --clear --backtitle "${BTITLE}" \
     --title "[ ¿Para qué quiero yo una Raspberry PI? ]" --menu "" 15 60 5 \
-    Date/time "Displays date and time" \
+    Desktop "Escritorio ligero LXDE" \
+    RPlay "AirPlay mirroring" \
     Sorteo "Sorteo Kit Raspberry Pi @raspipc" \
     Exit "Salir a la Shell" 2>"${INPUT}"
 
     menuitem=$(<"${INPUT}")
 
     case $menuitem in
-        Date/time) show_date;;
+        Desktop) startx;;
+        RPlay) RPLAY;;
         Sorteo) RandomNum; echo "Visita misapuntesde.com :)"; break;;
         Exit) echo "Visita misapuntesde.com :)"; break;;
     esac
